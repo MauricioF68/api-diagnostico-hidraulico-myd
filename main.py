26f3c2da26802bd38addb4459204f278d28ca7b9
MauricioF68 <mauricioterrones98@gmail.com>
2025-06-30 10:32:22 -0500

    Corrige el nombre del bucket de Firebase Storage v2

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ try:
         cred = credentials.Certificate(KEY_FILENAME)
 
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'diagnostico-piezas-myd.firebasestorage.app/reportes'
+        'storageBucket': 'diagnostico-piezas-myd.appspot.com'
     })
     db = firestore.client()
     bucket = storage.bucket()
